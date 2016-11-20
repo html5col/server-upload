17839f9b69df3c218f82f339c053a4e979325afd
html5col <html5master@sina.com>
2016-11-20 15:41:46 +0800

    remove false vip

--- controllers/user.js
+++ controllers/user.js
@@ -38,7 +38,7 @@ module.exports = {
 			User.find({
 				//'local.contractMoney': { $gt: 0,$lt: 100000},
 				'local.roles': { $in: ['Trial', 'Yearly'] },
-				'local.email': { $ne: '631738796@qq.com'},
+				'local.email': { $ne: '631738796@qq.com'}
 				
 				//'local.neVip': false,
 				//'local.neVip': {$ne: 'true'},
