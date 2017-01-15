786f35783ea70e3ffd961c9639dd04e80b1f5f4e
html5col <html5master@sina.com>
2017-01-15 08:38:28 +0800

    add examjple video

--- app.js
+++ app.js
@@ -100,10 +100,15 @@ function startServer(){
     // 	next();
     // });
 
-    let cors  = require('cors');
-    app.use(cors());
+    // let cors  = require('cors');
+    // app.use(cors());
 	
-	
+	// app.use(function(req, res, next) {
+	// res.header("Access-Control-Allow-Origin", "*");
+	// res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	// next();
+	// });
+
 	require('./routes')(app,passport,User);
     require('./part/autoView')(app);
 
