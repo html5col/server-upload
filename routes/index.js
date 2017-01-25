727437865c665e581fee38f969877934888ad29a
html5col <html5master@sina.com>
2017-01-25 11:42:14 +0800

    备案审核

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,13 @@ const main = require('../controllers/main'),
 
 
 /* GET home page. */
-// router.get('/', desktop.home);
+ router.get('/', function(req,res){
+     res.json('网站正在备案中...');
+ });
 router.get('/courses', desktop.home);
-router.get('/', post.latestTopic);
+//router.get('/', post.latestTopic);
 //router.get('/', main.simpleHome);
+router.get('/posts', post.latestTopic);
 
 router.get('/service',main.service);
 router.get('/rules',main.rules);
